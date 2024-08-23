9d44d71263f605e38cfd37f2072fdc74b4f17293
thehackingdev <nathanavaaga@gmail.com>
2024-08-23 19:49:57 +0300

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+# import Flask class from flask module
+from flask import Flask
+
+# create an instance called app of the class by passong the __name__ variable
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """display "Hello HBNB!"
+
+    Returns:
+        str: text on the index page
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""script starts a Flask web application"""
+
+
+# import Flask class from flask module
+from flask import Flask
+
+# create an instance called app of the class by passong the __name__ variable
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """display "Hello HBNB!"
+
+    Returns:
+        str: text on the index page
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb_route():
+    """display "HBNB"
+
+    Returns:
+        str: text on the page
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
